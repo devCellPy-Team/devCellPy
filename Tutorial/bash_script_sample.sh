22749953d5aa63b7c431136797f53496afb34b71
sidrayx <sidraxu@stanford.edu>
2022-06-22 21:59:22 -0700

    Tutorial updates

--- Tutorial/bash_script_sample.sh
+++ Tutorial/bash_script_sample.sh
@@ -22,8 +22,4 @@ devcellpy --runMode predictOne --rejectionCutoff 0.5 --predNormExpr /scratch/gro
 
 devcellpy --runMode predictAll --rejectionCutoff 0.5 --predNormExpr /scratch/groups/smwu/sidraxu/pbmc_10k_normalized.csv --layerObjectPaths /scratch/groups/smwu/sidraxu/cellpy_results_20210720155257/training/Root_object.pkl,/scratch/groups/smwu/sidraxu/cellpy_results_20210720155257/training/CD4_object.pkl,/scratch/groups/smwu/sidraxu/cellpy_results_20210720155257/training/CD8_object.pkl,/scratch/groups/smwu/sidraxu/cellpy_results_20210720155257/training/T-cell_object.pkl
 
-devcellpy --runMode predictOne --rejectionCutoff 0.5 --predNormExpr /scratch/groups/smwu/sidraxu/cardiac_normalized.csv --predMetadata /scratch/groups/smwu/sidraxu/cardiac_metadata.csv --layerObjectPaths cardiacDevAtlas --timePoint 10
-
-devcellpy --runMode predictAll --rejectionCutoff 0.5 --predNormExpr /scratch/groups/smwu/sidraxu/cardiac_normalized.csv --layerObjectPaths cardiacDevAtlas --timePoint 13
-
 devcellpy --runMode featureRankingOne --trainNormExpr /scratch/groups/smwu/sidraxu/zheng_pbmc_10K.csv --trainMetadata /scratch/groups/smwu/sidraxu/zheng_pbmc_10K_metadata.csv --layerObjectPaths /scratch/groups/smwu/sidraxu/cellpy_results_20210720155257/training/Root_object.pkl,/scratch/groups/smwu/sidraxu/cellpy_results_20210720155257/training/CD4_object.pkl,/scratch/groups/smwu/sidraxu/cellpy_results_20210720155257/training/CD8_object.pkl,/scratch/groups/smwu/sidraxu/cellpy_results_20210720155257/training/T-cell_object.pkl --featureRankingSplit 0.1
